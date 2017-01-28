ce22137f3bbd7638d5c37e6410c88b8dda089b58
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2017-01-28 23:01:15 +0100

    Sending of dummy values to influxdb

---
+++ config.sample.py
@@ -0,0 +1,15 @@
+# Configuration File
+
+# This is the sample configuration file, that I (Hermann) am using for my setup.
+# Copy it to 'config.py' and adapt it to your needs.
+
+# InfluxDB address
+influx_url = "http://gerty:8086/"
+
+influx_database_name = "aqua"
+
+# time interval for sending moisture measurements to the server (seconds)
+send_measurements_interval = 10  # seconds
+
+# number of measurements that should be aggregated before sending them to the server
+aggregated_measurements_count = 6  # default: 6 per minute, i.e. measuring every 10 seconds
---
+++ main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+from time import sleep, clock
+from pprint import pprint
+from statistics import median
+import json
+import requests
+import logging
+
+logger = logging.getLogger('hm-aquarium')
+
+try:
+    from config import *
+except ImportError as err:
+    print("The configuration file config.py does not exist. Have a look at config.sample.py for reference. (" +
+          str(err) + ")")
+    exit(1)
+
+
+def main():
+    print("Hm, Aquarium!")
+
+    # time between two measurements (seconds)
+    measure_interval = send_measurements_interval / aggregated_measurements_count
+
+    print("Measuring every", measure_interval, "seconds, aggregating", aggregated_measurements_count,
+          "values and sending them to the server every", send_measurements_interval, "seconds.")
+
+    values_count = 0
+    temperature_values = []
+
+    while True:
+        start_time = clock()
+        temperature_values.append(24.5)
+        values_count += 1
+        if values_count >= aggregated_measurements_count:
+            values_count = 0
+            median_temperature = median(temperature_values)
+            temperature_values = []
+            send(median_temperature)
+        processing_time = clock() - start_time
+        sleep_time = max(measure_interval - processing_time, 0)
+        sleep(sleep_time)
+
+
+def aggregate_values(values_map):
+    return {name: median(values) for name, values in values_map.items()}
+
+
+def send(temperature):
+    url = get_service_endpoint("write?db=" + influx_database_name)
+    headers = {}
+    data = "temp,name=water value=" + str(temperature)
+    print("Logging water temperature: " + str(temperature))
+    try:
+        response = requests.post(url, data=data, headers=headers, timeout=5.0)
+        response.raise_for_status()
+    except Exception as e:
+        logger.exception("Communication error with server", e)
+
+
+def get_service_endpoint(endpoint):
+    if influx_url.endswith("/"):
+        return influx_url + endpoint
+    else:
+        return influx_url + "/" + endpoint
+
+
+main()
