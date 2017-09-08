8290b95e3e538fdfc473eb2180a4e530a2d18804
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2017-09-08 16:45:20 +0200

    Stop video when page gets invisible

--- aquarium-web/src/app/camera/camera.component.ts
+++ aquarium-web/src/app/camera/camera.component.ts
@@ -14,9 +14,34 @@ export class CameraComponent implements OnInit {
 
   fullscreen = false;
 
+  webSocket: WebSocket = null;
+
   constructor() { }
 
   ngOnInit() {
+    document.addEventListener('visibilitychange', () => this.handleVisibilityChange(), false);
+    this.handleVisibilityChange();
+  }
+
+  handleVisibilityChange() {
+    if (document['hidden']) {
+      console.log('Stopping Video ' + this.websocketAddress);
+      this.stopVideo();
+    } else {
+      console.log('Starting Video ' + this.websocketAddress);
+      this.startVideo();
+    }
+  }
+
+  toggleFullscreen() {
+    this.fullscreen = !this.fullscreen;
+  }
+
+  startVideo() {
+    if (this.webSocket !== null) {
+      return; // video already started
+    }
+
     const target_fps = 24;
     const img = this.img;
 
@@ -28,41 +53,47 @@ export class CameraComponent implements OnInit {
     const request_time_smoothing = 0.2; // larger=more smoothing
     const target_time = 1000 / target_fps;
 
-    const ws = new WebSocket(this.websocketAddress);
-    ws.binaryType = 'arraybuffer';
+    this.webSocket = new WebSocket(this.websocketAddress);
+    this.webSocket.binaryType = 'arraybuffer';
 
-    function requestImage() {
-        request_start_time = performance.now();
-        ws.send('more');
+    const requestImage = () => {
+      request_start_time = performance.now();
+      if (this.webSocket !== null) {
+        this.webSocket.send('more');
+      }
     }
 
-    ws.onopen = function() {
-        start_time = performance.now();
-        requestImage();
+    this.webSocket.onopen = () => {
+      start_time = performance.now();
+      requestImage();
     };
 
-    ws.onmessage = function(evt) {
-        const arrayBuffer = evt.data;
-        const blob  = new Blob([new Uint8Array(arrayBuffer)], {type: 'image/jpeg'});
-        img.nativeElement.src = window.URL.createObjectURL(blob);
+    this.webSocket.onmessage = (evt) => {
+      const arrayBuffer = evt.data;
+      const blob = new Blob([new Uint8Array(arrayBuffer)], { type: 'image/jpeg' });
+      img.nativeElement.src = window.URL.createObjectURL(blob);
 
-        const end_time = performance.now();
-        const current_time = end_time - start_time;
-        // smooth with moving average
-        time = (time * time_smoothing) + (current_time * (1.0 - time_smoothing));
-        start_time = end_time;
+      const end_time = performance.now();
+      const current_time = end_time - start_time;
+      // smooth with moving average
+      time = (time * time_smoothing) + (current_time * (1.0 - time_smoothing));
+      start_time = end_time;
 
-        const current_request_time = performance.now() - request_start_time;
-        // smooth with moving average
-        request_time = (request_time * request_time_smoothing) + (current_request_time * (1.0 - request_time_smoothing));
-        const timeout = Math.max(0, target_time - request_time);
+      const current_request_time = performance.now() - request_start_time;
+      // smooth with moving average
+      request_time = (request_time * request_time_smoothing) + (current_request_time * (1.0 - request_time_smoothing));
+      const timeout = Math.max(0, target_time - request_time);
 
-        setTimeout(requestImage, timeout);
+      setTimeout(requestImage, timeout);
     };
   }
 
-  toggleFullscreen() {
-    this.fullscreen = !this.fullscreen;
-  }
+  stopVideo() {
+    if (this.webSocket === null) {
+      return; // video already stopped
+    }
 
+    this.webSocket.close();
+    this.webSocket = null;
+  }
 }
